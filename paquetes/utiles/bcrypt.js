3e81dad0cf0f57f9886637854a551b99b1956d50
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-28 17:07:04 -0500

    Clase 22 - Módulos útiles

---
+++ paquetes/utiles/bcrypt.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+
+const password = '1234Segura!';
+
+bcrypt.hash(password, 10, (err, hash) => {
+  console.log(hash);
+
+  bcrypt.compare(password, hash, (err, res) => {
+    console.log(err);
+    console.log(res);
+  });
+});
